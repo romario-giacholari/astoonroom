1b8194358d7bbf5b9041b9a64f64731a2f545281
Romario Giacholari <exit>
2017-01-02 21:05:16 +0000

    New layout

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.app')
 @section('content')
-@if(count($articles)>0)
 <div class = 'container'>
 <div class = 'row'>
 <div class = "col-md-12 col-md-offset-0" >
@@ -65,9 +64,9 @@ $(document).ready(function(){
 				           <form method = 'GET' action = 'articles/search'>
 							
 							  <div class = "col-md-2 col-md-offset-0">
-								<div class="form-group">
+								<div class="form-group" >
 								  <label for="sel1">Sort by location</label>
-								  <select class="form-control" id="q" name = 'q'>
+								  <select required class="form-control" id="q" name = 'q'>
 								    <option>Lakeside</option>
 								    <option>William Murdoch</option>
 								    <option>Harriet Martineau</option>
@@ -78,7 +77,7 @@ $(document).ready(function(){
 							  </div>
 
 								 <div class = "col-md-12 col-md-offset-0">
-								  	<button  class = 'btn btn-info'>Search</button>
+								  	<input type = 'submit'  class = 'btn btn-info'>
 								  </div>
 								
 								
@@ -89,33 +88,38 @@ $(document).ready(function(){
         
 
 @foreach($articles as $article)
+<a href = '{{$article->title}}'>
+<div class = "col-md-12 col-md-offset-0 jumbotron" id ="Posts"  style = 'margin-top:10px' >
+	
+		 <div class="col-md-3 col-md-offset-0 form-group">
+		 <div>Views: {{$article->views}}</div>
+		 <div>Year: {{$article->year}}</div>
+		  <div >Contact info: 
+		     @if($article->contact)
+		     Provided
+		     @else
+		     None given
+		     @endif
+		     </div>
+		     <div>Location: {{$article->location}}</div>
+		     <div>Gender: {{$article->gender}}</div>
+		     <div> {{$article->created_at->diffForHumans()}}</div>
+		    <label style = 'width:100%;font-family:Arial, Helvetica, sans-serif;'><h2>{{$article->title}}</h2></label>
+		 </div>
+	
+		@if(count($article->photo) > 0)
+	     <div class = "col-md-9 col-md-offset-0">
+		 	<div class = 'row'>
+				<img class = 'img-responsive' src = '{{$article->photo[0]->path}}' height = "250px" width="230px" >
+			</div>
+		 </div>
+		@endif
+</div>
 
-<a href = 'articles/{{$article->title}}' >
-<div class=" col-md-12" id ='Posts' >
-<div class = 'row'>
-	     
-			
-			@if(count($article->photo) > 0 )
-			<img class ='img-responsive' src = '{{$article->photo[0]->path}}'>
-			@endif
+</a>
 
-
-		  	<div id = 'banner' class = 'img-thumnail' style = 'font-size:2em;margin-top:0px;background-color:#F0F5F5;min-width: 300px; padding:15px'>
-				<div class = 'row' style = 'padding:40px'>
-					<h3 style = 'font-family:Arial, Helvetica, sans-serif'>{{$article->title}}</h3>
-					<div>{{$article->location}}</div>
-					<span class="glyphicon glyphicon-eye-open"></span> {{$article->views}} 
-		 	   </div>
-		   </div>
-		 
-		     
-		
-		 
-	</div>
-	</div>
-	</a> 
-	@endforeach
-
+  
+@endforeach
 	</div>
 		
 </div>
@@ -126,13 +130,4 @@ $(document).ready(function(){
 
 </div>
 <div class = 'text-center'>{{$articles->links()}}</div>
-@else
-<div class = 'container'>
-	<div class = 'row'>
-		<div class = "col-md-12 col-md-offset-0" >
-			<div class = 'jumbotron'><h1 style = 'font-family:Lato'>No ads have been placed yet</h1> </div>
-		</div>
-	</div>
-</div>		
-@endif
 @endsection
