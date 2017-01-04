de3f765f339f8c9d2262bc83a40109497c80c072
Romario Giacholari <exit>
2017-01-04 22:29:58 +0000

    configuring s3

--- config/aws.php
+++ config/aws.php
@@ -6,8 +6,8 @@ use Aws\Laravel\AwsServiceProvider;
 return [
     'credentials' => [
         
-        'key'    => getenv('S3_KEY'),
-        'secret' => getenv('S3_SECRET'),
+        'key'    => 'AWS_KEY',
+        'secret' => 'AWS_SECRET',
         'region' => 'eu-west-2',
         'bucket' => 'aston-room',
         'version' => 'latest',
